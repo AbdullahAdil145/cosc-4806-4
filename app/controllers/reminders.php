cbbc9b081791a6e1b12dfcabfa42a3cc828d034f
Muhammad Abdullah Adil <abdullahadil143@gmail.com>
2025-07-05 01:05:29 +0000

    added CRUD to controller

--- app/controllers/reminders.php
+++ app/controllers/reminders.php
@@ -1,9 +1,48 @@
 <?php
 
 class Reminders extends Controller {
+
     public function index() {
         $reminder = $this->model('Reminder');
         $list_of_reminders = $reminder->get_all_reminders();
         $this->view('reminders/index', ['reminders' => $list_of_reminders]);
+    }
+
+    public function create() {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $reminder = $this->model('Reminder');
+            $user_id = $_POST['user_id'] ?? null;
+            $subject = $_POST['subject'] ?? '';
+
+            if ($user_id && $subject) {
+                $reminder->create_reminder($user_id, $subject);
+                header("Location: /reminders/index");
+                exit;
+            }
+        }
+    }
+
+    public function update($id) {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $reminder = $this->model('Reminder');
+            $subject = $_POST['subject'] ?? '';
+
+            if ($subject) {
+                $reminder->update_reminder($id, $subject);
+                header("Location: /reminders/index");
+                exit;
+            }
+        }
+    }
+
+    public function delete($id) {
+        $reminder = $this->model('Reminder');
+        $reminder->delete_reminder($id);
+        header("Location: /reminders/index");
+        exit;
+    }
+
+    public function complete($id) {
+        $this->delete($id);
     }
 }
