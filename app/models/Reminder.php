08d16d62738a95111968d99ff4d57f54fd12dea5
Muhammad Abdullah Adil <abdullahadil143@gmail.com>
2025-07-05 01:04:04 +0000

    added CRUD to reminder model

--- app/models/Reminder.php
+++ app/models/Reminder.php
@@ -5,11 +5,34 @@ class Reminder {
     public function __construct() {
     }
 
-    public function get_all_reminders () {
+    public function get_all_reminders() {
         $db = db_connect();
-        $statement = $db->prepare("select * from reminders;");
+        $statement = $db->prepare("SELECT * FROM reminders;");
         $statement->execute();
         $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
         return $rows;
+    }
+
+    public function create_reminder($user_id, $subject) {
+        $db = db_connect();
+        $statement = $db->prepare("INSERT INTO reminders (user_id, subject, created_at) VALUES (?, ?, NOW());");
+        return $statement->execute([$user_id, $subject]);
+    }
+
+    public function update_reminder($id, $subject) {
+        $db = db_connect();
+        $statement = $db->prepare("UPDATE reminders SET subject = ? WHERE id = ?;");
+        return $statement->execute([$subject, $id]);
+    }
+
+    public function delete_reminder($id) {
+        $db = db_connect();
+        $statement = $db->prepare("DELETE FROM reminders WHERE id = ?;");
+        return $statement->execute([$id]);
+    }
+
+    // Complete reminder is just a delete
+    public function complete_reminder($id) {
+        return $this->delete_reminder($id);
     }
 }
