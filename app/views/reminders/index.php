be68b4e5452c8de8f7742ad27ccedfa68c3e9023
Muhammad Abdullah Adil <abdullahadil143@gmail.com>
2025-07-05 00:16:28 +0000

    Add a basic reminder page

---
+++ app/views/reminders/index.php
@@ -0,0 +1,14 @@
+<? require_once 'app/views/templates/header.php';
+ ?>
+<section class="reminders-section">
+  <h2 class="section-heading" style="text-align: center;">⏰ Reminders</h2>
+  <div class="reminders-wrapper">
+    <div class="reminder-card">
+      <p>This is a reminder.</p>
+      <p class="reminder-date">12/12/2023</p>
+      <button class="reminder-button">Complete</button>
+      <button class="reminder-button">Update</button>
+      <button class="reminder-button">Delete</button>
+      </div>
+
+<?php require_once 'app/views/templates/footer.php'?>
