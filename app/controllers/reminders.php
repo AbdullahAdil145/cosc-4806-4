36ed22d0067b92aa5c22d2c7c1216d2f3fda4940
Muhammad Abdullah Adil <abdullahadil143@gmail.com>
2025-07-05 00:32:59 +0000

    update reminder controller

--- app/controllers/reminders.php
+++ app/controllers/reminders.php
@@ -2,6 +2,8 @@
 
 class Reminders extends Controller {
     public function index() {
+        $reminder = $this->model('Reminder');
+        $data = get_all_reminders->test();
         $this->view('reminders/index');
     }
 }
